5c31502d0abc1720aefa004bfef98e9a14ade26c
linhyo <linh.yolily@gmail.com>
2014-11-25 23:53:59 +0700

    ...

---
+++ js/revolutionslider/rs-plugin/js/custom.js
@@ -0,0 +1,15 @@
+var revapi;
+
+jQuery(document).ready(function() {
+
+	   revapi = jQuery('.tp-banner').revolution(
+		{
+			delay:5000,
+			startwidth:1170,
+			startheight:500,
+			hideThumbs:10,
+			fullWidth:"on",
+			forceFullWidth:"on"
+		});
+
+});
